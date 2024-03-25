fe92b15ffccc2d680c38235290e354f93c33b0c8
sajal <Jainsajal519@gmail.com>
2024-03-25 22:18:29 +0530

    Small Routes

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const http= require ('http');
 const server=http.createServer((req,res) =>{
-  res.end("Noice");
+  if(req.url==='/about')
+  res.end('<h1>About</h1>');
+  else   if(req.url==='/home')
+  res.end('<h1>Home</h1>');
+  else
+  res.end('<h1>Not Found</h1>');
 });
 server.listen(5001, () =>
 {
