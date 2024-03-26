b6083acb9d697b9f1f7c483273d9b7909270becf
sajal <Jainsajal519@gmail.com>
2024-03-26 23:55:06 +0530

    free commits

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const server=http.createServer((req,res) =>{
   res.end('<h1>About</h1>');
   else   if(req.url==='/home')
   res.end('<h1>Home</h1>');
-  else
+  else if(req.url==='/big')
+  res.end('<h1>Not Found</h1>');
+    else if(req.url==='/small')
+  res.end('<h1>Not Found</h1>');
+    else if(req.url==='/ok')
+  res.end('<h1>Not Found</h1>');
+    else 
   res.end('<h1>Not Found</h1>');
 });
 server.listen(5001, () =>
