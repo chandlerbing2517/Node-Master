dd4df47e0d9b66135496414808626daeaad01d8d
sajal <Jainsajal519@gmail.com>
2024-04-07 23:37:41 +0530

    free

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const server=http.createServer((req,res) =>{
   res.end('<h1>About</h1>');
   else   if(req.url==='/home')
   res.end('<h1>Home</h1>');
+    else   if(req.url==='/home1')
+  res.end('<h1>Home</h1>');
+    else   if(req.url==='/home2')
+  res.end('<h1>Home</h1>');
     else 
   res.end('<h1>Not Found</h1>');
 });
